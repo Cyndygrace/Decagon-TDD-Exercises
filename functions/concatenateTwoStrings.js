eb2e6214fe113817dbf6ae430935e3c0dc100002
Cynthia Iloekwe <cyniloekwe@gmail.com>
2019-04-18 04:24:56 +0100

    corrected the first if statment for my function that concatenate two strings

--- functions/concatenateTwoStrings.js
+++ functions/concatenateTwoStrings.js
@@ -1,5 +1,5 @@
 function concatenate(firstString, secondString) {
-    if (!firstNumber && !secondNumber){
+    if (!firstString && !secondString){
         return "Enter two words"
     }
     if (typeof firstString === "string" && typeof secondString === "string"){
