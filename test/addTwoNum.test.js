e88f83ff4ad3fa433a8887a63f1c83bbf052e749
Cynthia Iloekwe <cyniloekwe@gmail.com>
2019-04-18 03:45:53 +0100

    added test that the addition of only one input parameter should output invalid input

--- test/addTwoNum.test.js
+++ test/addTwoNum.test.js
@@ -7,3 +7,6 @@ test ("that the addition of 30 and 50 outputs 80", () =>{
 
     expect(add2(30, 50)).toBe(80)
 });
+test ("that the addition of only one input parameter should output invalid input", () =>{
+    expect(add2(3)).toBe("invalid input")
+});
