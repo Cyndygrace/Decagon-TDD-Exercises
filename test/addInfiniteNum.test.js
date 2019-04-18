15408359afcb86540e75c6b946f5fde30be0cb6d
Cynthia Iloekwe <cyniloekwe@gmail.com>
2019-04-18 03:23:51 +0100

    added test case to output Enter a number if there is no input parameter

--- test/addInfiniteNum.test.js
+++ test/addInfiniteNum.test.js
@@ -8,4 +8,9 @@ test("that if argument input contains a combination of boolean type, string type
     expect(add("false", "dog", 8, 9, "indefinite")).toEqual("invalid input")
 });
 
-
+test ("to return Enter a number if there is no input parameter", () => {
+    expect(add()).toEqual("Enter a number")
+});
+test ("that an input of only string will output invalid input", () => {
+    expect(add("string", "stringify", "last")).toEqual("invalid input")
+});
