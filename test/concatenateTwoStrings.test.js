ab2ef630402528d6e57cea2aee77c8d4cd5fe636
Cynthia Iloekwe <cyniloekwe@gmail.com>
2019-04-18 04:12:08 +0100

    test that the addition of two strings will concatenate them

--- test/concatenateTwoStrings.test.js
+++ test/concatenateTwoStrings.test.js
@@ -1,2 +1,8 @@
 const concatenate = require('../functions/concatenateTwoStrings');
 
+test("that the addition of two strings will concatenate them ", () =>{
+    expect(concatenate("coding", "fun")).toBe("codingfun")
+});
+test("that big house output bighouse ", () =>{
+    expect(concatenate("big", "house")).toBe("bighouse")
+});
