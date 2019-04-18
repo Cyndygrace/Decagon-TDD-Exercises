efe691ab94cdae767c8a3b7fde24510cd45feaaa
Cynthia Iloekwe <cyniloekwe@gmail.com>
2019-04-18 04:13:30 +0100

    that a function with an empty parameters should output Enter Two words

--- test/concatenateTwoStrings.test.js
+++ test/concatenateTwoStrings.test.js
@@ -1,8 +1,11 @@
 const concatenate = require('../functions/concatenateTwoStrings');
 
-test("that the addition of two strings will concatenate them ", () =>{
+test("that the concatenation of two strings will concatenate them ", () =>{
     expect(concatenate("coding", "fun")).toBe("codingfun")
 });
 test("that big house output bighouse ", () =>{
     expect(concatenate("big", "house")).toBe("bighouse")
+});
+test ("that a function with an empty parameters should output Enter Two words", () =>{
+    expect(add2()).toBe("Enter two words")
 });
