768b3cf2658f7ef9a606f1353f8f44ad029afcb4
Cynthia Iloekwe <cyniloekwe@gmail.com>
2019-04-18 04:22:16 +0100

    added a function that concatenate two strings

---
+++ functions/concatenateTwoStrings.js
@@ -0,0 +1,11 @@
+function concatenate(firstString, secondString) {
+    if (!firstNumber && !secondNumber){
+        return "Enter two words"
+    }
+    if (typeof firstString === "string" && typeof secondString === "string"){
+        return firstString + secondString
+    } else {
+    return "invalid input"
+     }
+}
+
