44f9068090766fc28a30c5846737826cacfd69f3
Cynthia Iloekwe <cyniloekwe@gmail.com>
2019-04-18 03:20:23 +0100

    added test case that if argument input contains a combination of boolean type, string type, numbers and indefinite, it should output invalid input

--- test/addInfiniteNum.test.js
+++ test/addInfiniteNum.test.js
@@ -4,4 +4,8 @@ test("that it returns the sum of an indefinite number", () => {
     expect(add(3, 5, 8, 5)).toEqual(21)
 });
 
+test("that if argument input contains a combination of boolean type, string type, numbers and indefinite, it should output invalid input", () => {
+    expect(add("false", "dog", 8, 9, "indefinite")).toEqual("invalid input")
+});
+
 
